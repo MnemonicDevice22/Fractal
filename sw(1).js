f763367f84ec01e551c427baa201a7ad0a2b11a4
MnemonicDevice22 <164077262+MnemonicDevice22@users.noreply.github.com>
2026-09-16 14:13:55 -0600

    Add files via upload

---
+++ sw(1).js
@@ -0,0 +1,39 @@
+const APP     = "fractal";
+const VERSION = 1;
+const CACHE   = `${APP}-v${VERSION}`;
+
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.webmanifest",
+  "./icon-512.png",
+  "./maskable-512.png",
+  "./font.woff2"
+];
+
+self.addEventListener("install", e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
+});
+
+self.addEventListener("activate", e => {
+  e.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(
+        keys.filter(k => k.startsWith(APP) && k !== CACHE).map(k => caches.delete(k))
+      ))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch", e => {
+  if (e.request.method !== "GET") return;
+  e.respondWith(
+    fetch(e.request, { cache: "no-store" })
+      .then(res => {
+        const copy = res.clone();
+        caches.open(CACHE).then(c => c.put(e.request, copy)).catch(() => {});
+        return res;
+      })
+      .catch(() => caches.match(e.request).then(r => r || caches.match("./index.html")))
+  );
+});
